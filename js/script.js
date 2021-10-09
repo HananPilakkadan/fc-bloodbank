d415fdff8aee5422fbe69d1fc0c5b60bdb9ca8c9
Hanan <63251521+HananPilakkadan@users.noreply.github.com>
2021-10-10 01:13:40 +0530

    fc-blood-bank

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var overlay = document.getElementById("overlay");
+    overlay.classList.remove("show");
+    var loginPage = document.getElementById("login");
+    loginPage.classList.remove("show");
+    document.body.style.overflow = 'visible';
+  }
